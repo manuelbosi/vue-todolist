96ddc3ba7a23c07a9536ff94581642efb728e0eb
Manuel Bosi <manuelbosi57@gmail.com>
2020-11-18 14:38:05 +0100

    inizio inserimento funzione elimina todo

--- js/app.js
+++ js/app.js
@@ -10,12 +10,20 @@ const app = new Vue({
     todo: null
   },
   methods: {
-    addTodo(){ // funzione per aggiungere task
+    
+    // funzione per aggiungere task
+    addTodo(){
       console.log("TASK DA AGGIUNGERE", this.todo) // debug: vedo se il valore che mi stampa è quello dell'input
       this.taskList.push(this.todo) // pusho il valore inserito nell'array
       console.log("ARRAY", this.taskList) // debug: vedo se il valore è stato inserito correttamente
+    },
 
+    // funzione per eliminare task
+    deleteTodo(){
+      console.log("clicked");
     }
+
+
   } 
 });
 
@@ -25,3 +33,4 @@ const app = new Vue({
 // un array inizializzato vuoto che contiene tutte le task aggiunte - OK
 // una funzione sul bottone aggiungi che pusha il valore dell'input dentro l'array - OK
 // una serie di li generati dinamicamente con un v-for che stampa ogni valore dell'array - OK
+// una funzione sulla X per eliminare li corrispondente che si vuole cancellare
