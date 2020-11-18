90fef9c7cb614f4874a7b2b7fb6be84f090425b5
Manuel Bosi <manuelbosi57@gmail.com>
2020-11-18 14:12:47 +0100

    inserimento consegna esercizio

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+// Creiamo una mini app di todolist con html,css,js in VUE
+// Un input permette all’utente di scrivere una “cosa da fare”;
+// Un bottone permette di aggiungere quella cosa alla lista di cose da fare in pagina, come promemoria;
+// Un bottone (X) per eliminare quella determinata cosa dalla lista.
+
 const app = new Vue({
   el: "#app",
   data: {
